d0ac318ae84dc827df18b177e9196db648b25b2a
ArtiomB5 <74674499+ArtiomB5@users.noreply.github.com>
2021-06-11 22:09:39 +0300

    Add files via upload

---
+++ script/dist/main.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var playPauseButton = document.querySelector('.playPause');
+playPauseButton.addEventListener('click', playPauseFunc);
+
+function playPauseFunc(event) {
+  var mainPageBgVideo = document.querySelector('.bgvideo');
+
+  if (mainPageBgVideo.paused === false) {
+    mainPageBgVideo.pause();
+    event.currentTarget.innerHTML = '<span class="material-icons">play_arrow</span>';
+  } else if (mainPageBgVideo.paused === true) {
+    mainPageBgVideo.play();
+    event.currentTarget.innerHTML = '<span class="material-icons">pause</span>';
+  }
+}
